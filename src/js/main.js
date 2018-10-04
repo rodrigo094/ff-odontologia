37345dfacf38272561243972a69b73d10520a9ae
Rodrigo Oliveira Araujo <rodrigo.araujo@enext.local>
2018-10-04 18:47:52 -0300

    Scroll Suave

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-
 import $ from 'jquery'
 import jQuery from 'jquery'
 
@@ -6,50 +5,61 @@ window.$ = $
 window.jQuery = jQuery
 
 function jarallaxPlay() {
-	$('.jarallax').jarallax({
-	    speed: 0.2
-	});
+    $('.jarallax').jarallax({
+        speed: 0.2
+    });
 };
 
 function jarallaxPlayBottom() {
-	$('.content__parallax .jarallax').jarallax({
-	    speed: 0.2
-	});
+    $('.content__parallax .jarallax').jarallax({
+        speed: 0.2
+    });
 };
 
 function slickActive() {
-	$(".content__middle--instagram ul").slick({
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  infinite: true,
-	  autoplay: true,
-	  arrows: false,
-	  speed: 500,
-	  fade: true,
-	  cssEase: 'linear'
-	});
+    $(".content__middle--instagram ul").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        arrows: false,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
 };
 
-function openMenuMobile(){
-	$('.header__menu-mobile--hamburger').click(function(){
-		$('.header__pos--menu-mobile--items').addClass('active');
-		$('.header__pos--menu-mobile--close').addClass('active');
-		$('.header__pos--menu-mobile--background').addClass('active');
-	});
+function scrollPage() {
+    var $doc = $('html, body');
+    $('a').click(function() {
+        $doc.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+        return false;
+    });
 };
 
-function closeMenuMobile(){
-	$('.header__pos--menu-mobile--close').click(function(){
-		$('.header__pos--menu-mobile--items').removeClass('active');
-		$('.header__pos--menu-mobile--background').removeClass('active');
-		$('.header__pos--menu-mobile--close').removeClass('active');
-	});
+function openMenuMobile() {
+    $('.header__menu-mobile--hamburger').click(function() {
+        $('.header__pos--menu-mobile--items').addClass('active');
+        $('.header__pos--menu-mobile--close').addClass('active');
+        $('.header__pos--menu-mobile--background').addClass('active');
+    });
 };
 
-$(document).ready(function(){
-	jarallaxPlay();
-	slickActive();
-	jarallaxPlayBottom();
-	openMenuMobile();
-	closeMenuMobile();
+function closeMenuMobile() {
+    $('.header__pos--menu-mobile--close').click(function() {
+        $('.header__pos--menu-mobile--items').removeClass('active');
+        $('.header__pos--menu-mobile--background').removeClass('active');
+        $('.header__pos--menu-mobile--close').removeClass('active');
+    });
+};
+
+$(document).ready(function() {
+    jarallaxPlay();
+    slickActive();
+    jarallaxPlayBottom();
+    openMenuMobile();
+    closeMenuMobile();
+    scrollPage();
 });
